97abb78dd3e20556d6402003ba71daa6aed27cdc
Hanmin <rlemdhs2747@naver.com>
2026-06-29 11:25:48 +0900

    ✨ feat: Security 설정 파일 추가

---
+++ src/main/java/com/hanmin/loafy/global/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.hanmin.loafy.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final String[] allowUrl = {
+
+    };
+
+    public SecurityFilterChain filterChain(HttpSecurity http,
+                                           @Qualifier("apiConfigurationSource") CorsConfigurationSource corsConfigurationSource)
+            throws Exception {
+        http.authorizeHttpRequests(request ->
+                request.requestMatchers(allowUrl).permitAll().anyRequest().authenticated());
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
