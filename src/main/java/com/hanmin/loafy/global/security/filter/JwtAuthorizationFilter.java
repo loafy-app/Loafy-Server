92e581b5ec7ede22c7dc55aba286e1f164a42ba5
Hanmin <rlemdhs2747@naver.com>
2026-06-30 18:34:55 +0900

    ✨ feat: JWT 검증 필터 구현 및 Security 설정에 적용

---
+++ src/main/java/com/hanmin/loafy/global/security/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,79 @@
+package com.hanmin.loafy.global.security.filter;
+
+import com.hanmin.loafy.global.security.auth.CustomUserDetails;
+import com.hanmin.loafy.global.security.auth.CustomUserDetailsService;
+import com.hanmin.loafy.global.security.jwt.JwtUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.security.SignatureException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    protected void doFilterInternal (
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws IOException, ServletException {
+        String uri = request.getRequestURI();
+        if (uri.startsWith("/swagger") || uri.startsWith("v3/api-docs")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        log.info("[ JwtAuthorizationFilter ]: 인가 필터 작동");
+
+        try {
+            String accessToken = jwtUtil.resolveAccessToken(request);
+            if (accessToken == null) {
+                log.info("[ JwtAuthorizationFilter ]: AccessToken이 존재하지 않습니다. 필터를 건너뜁니다.");
+                filterChain.doFilter(request, response);
+                return;
+            }
+            authenticateAccessToken(accessToken);
+            log.info("[ JwtAuthorizationFilter ]: 다음 필터로 넘어갑니다.");
+            filterChain.doFilter(request, response);
+
+        } catch (ExpiredJwtException e) {
+            log.warn("[ JwtAuthorizationFilter ]: 토큰이 만료되었습니다.");
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write("AccessToken이 만료되었습니다.");
+        } catch (SignatureException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void authenticateAccessToken(String accessToken) throws SignatureException {
+        log.info("[ JwtAuthorizationFilter ]: 토큰으로 인가 과정을 시작합니다.");
+        jwtUtil.validateToken(accessToken);
+        log.info("[ JwtAuthorizationFilter ]: AccessToken 유효성 검증 성공");
+        String email = jwtUtil.getEmail(accessToken);
+        CustomUserDetails customUserDetails = (CustomUserDetails)customUserDetailsService.loadUserByUsername(email);
+        log.info("[ JwtAuthorizationFilter ]: 객체 생성 성공");
+        Authentication authToken = new UsernamePasswordAuthenticationToken(
+                customUserDetails,
+                null,
+                customUserDetails.getAuthorities()
+        );
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+        log.info("[ JwtAuthorizationFilter ]: 인증 객체 저장 완료");
+    }
+
+}
