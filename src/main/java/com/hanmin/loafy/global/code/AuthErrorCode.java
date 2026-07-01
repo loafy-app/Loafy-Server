121dbbdcca8bf97d8bfd9913dace117dee04d52f
Hanmin <rlemdhs2747@naver.com>
2026-07-01 11:29:46 +0900

    🔧 chore: AuthErrorCode 상태 코드 순서 변경

--- src/main/java/com/hanmin/loafy/global/code/AuthErrorCode.java
+++ src/main/java/com/hanmin/loafy/global/code/AuthErrorCode.java
@@ -9,8 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum AuthErrorCode implements BaseErrorCode{
 
     BAD_REQUEST_400(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
-    FORBIDDEN_403(HttpStatus.FORBIDDEN, "COMMON403", "접근이 금지되었습니다."),
     UNAUTHORIZED_401(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다."),
+    FORBIDDEN_403(HttpStatus.FORBIDDEN, "COMMON403", "접근이 금지되었습니다."),
     NOT_FOUND_404(HttpStatus.NOT_FOUND, "COMMON404", "요청한 자원을 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
