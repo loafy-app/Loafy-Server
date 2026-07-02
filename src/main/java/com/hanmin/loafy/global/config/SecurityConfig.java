fff84392f8cf0501172278b9267f8bd5387b24c2
Hanmin <rlemdhs2747@naver.com>
2026-07-02 18:06:57 +0900

    🔧 chore: AuthenticationManager에서 throw되지 않는 예외 삭제

--- src/main/java/com/hanmin/loafy/global/config/SecurityConfig.java
+++ src/main/java/com/hanmin/loafy/global/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) {
         return configuration.getAuthenticationManager();
     }
 
