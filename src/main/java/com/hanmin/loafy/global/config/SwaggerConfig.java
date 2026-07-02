32e6059cefc2e399555d25ce99ae4096097b6d6e
Hanmin <rlemdhs2747@naver.com>
2026-07-02 14:01:02 +0900

    🔧 chore: gradle에 swagger 종속성 추가

---
+++ src/main/java/com/hanmin/loafy/global/config/SwaggerConfig.java
@@ -0,0 +1,11 @@
+package com.hanmin.loafy.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    publi
+}
