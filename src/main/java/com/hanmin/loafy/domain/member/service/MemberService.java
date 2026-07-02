5e00023de93dac5e80c617f3f7f06f3b7c197aad
Hanmin <rlemdhs2747@naver.com>
2026-07-02 18:00:32 +0900

    🐛 bug: 회원 중복 가입 오류 응답 수정

--- src/main/java/com/hanmin/loafy/domain/member/service/MemberService.java
+++ src/main/java/com/hanmin/loafy/domain/member/service/MemberService.java
@@ -26,7 +26,7 @@ public class MemberService {
         Member member = MemberConverter.toMember(request, passwordEncoder);
         if (memberRepository.findByEmail(request.email()).isPresent()) {
             log.info("[ MemberService ]: 이미 존재하는 회원입니다.");
-            throw new MemberException(AuthErrorCode.BAD_REQUEST_400);
+            throw new MemberException(AuthErrorCode.CONFLICT_409);
         }
         else {
             memberRepository.save(member);
