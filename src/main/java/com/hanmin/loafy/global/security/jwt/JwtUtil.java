1a3bc166857fe80405c838e1a4b286b4a05a778c
Hanmin <rlemdhs2747@naver.com>
2026-07-06 13:21:56 +0900

    ♻️ refactor: validate token에서 isExpired 제거

--- src/main/java/com/hanmin/loafy/global/security/jwt/JwtUtil.java
+++ src/main/java/com/hanmin/loafy/global/security/jwt/JwtUtil.java
@@ -44,7 +44,7 @@ public class JwtUtil {
     }
 
     // SignatureException: jwt의 signature값의 불일치로 발생하는 예외
-    public String getEmail(String token) throws SignatureException {
+    public String getEmail(String token) {
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getSubject();
     }
 
@@ -118,8 +118,11 @@ public class JwtUtil {
         log.info("[ JwtUtil ]: 토큰의 유효성을 검증합니다.");
         try {
             long seconds = 3 * 60;
-            boolean isExpired  = Jwts.parser().clockSkewSeconds(seconds).verifyWith(secretKey).build()
-                    .parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+            Jwts.parser()
+                    .clockSkewSeconds(seconds)
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token);
         } catch (ExpiredJwtException e) {
             log.warn("[ JwtUtil ]: 만료된 JWT 토큰입니다.");
             throw new ExpiredJwtException(null, null, "만료된 JWT 토큰입니다.");
